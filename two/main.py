5a3e0379b312afcb13df08c438368b2277c86a5e
BashkirovaNatalya <67271896+BashkirovaNatalya@users.noreply.github.com>
2021-11-24 15:07:47 +0500

    added scatter plots

--- two/main.py
+++ two/main.py
@@ -1,4 +1,7 @@
+from matplotlib.markers import MarkerStyle
 import pandas as pd
+from matplotlib import pyplot
+
 
 df = pd.read_csv('two\weight-height.csv')
 
@@ -8,5 +11,15 @@ df['Weight'] = df['Weight']/2.205
 
 female_df = df[df['Gender'] == 'Female']
 male_df = df[df['Gender'] == 'Male']
+
+pyplot.scatter(female_df['Height'], female_df['Weight'], c='pink', marker=".")
+pyplot.show()
+pyplot.scatter(male_df['Height'], male_df['Weight'], c='blue', marker=".")
+pyplot.show()
+
+
+pyplot.scatter(female_df['Height'], female_df['Weight'], c='pink', marker=".")
+pyplot.scatter(male_df['Height'], male_df['Weight'], c='blue', marker=".")
+pyplot.show()
 
 print(female_df.head())
