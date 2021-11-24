556b4ebe15d410e92dc883cd3a2fdbcaf398a704
BashkirovaNatalya <67271896+BashkirovaNatalya@users.noreply.github.com>
2021-11-24 14:38:49 +0500

    converted to cm and kg

--- two/main.py
+++ two/main.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 df = pd.read_csv('two\weight-height.csv')
 
+df['Height'] = df['Height']*2.54
+df['Weight'] = df['Weight']/2.205
+
+
 female_df = df[df['Gender'] == 'Female']
 male_df = df[df['Gender'] == 'Male']
 
+print(female_df.head())
